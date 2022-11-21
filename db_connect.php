ebbf28e7f16d0c890cb8cc22d93fdea5e9a6880d
unknown <jason.lionardi@binus.ac.id>
2022-11-21 11:37:02 -0800

    test with vscode

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,1 @@
 <?php $db_connect = new mysqli("localhost","root","","secprog_mid"); ?>
-
-<!--QUERY:-->
-<!---->
-<!--CREATE TABLE `User` (-->
-<!--ID INT AUTO_INCREMENT PRIMARY KEY,-->
-<!--Username VARCHAR(255),-->
-<!--Password VARCHAR(50)-->
-<!--);-->
-<!---->
-<!--INSERT INTO `User` (Username, Password) VALUES-->
-<!--('Admin', 'ae0bd9f322740f14e978c1040c137d46a2bf4a3b'),-->
-<!--('Maintainer', 'bb52f0e5030b95e89341de51e1966909daa645c1');-->
